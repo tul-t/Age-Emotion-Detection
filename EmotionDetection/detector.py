7e0f942719b971465aa7b3733df0c2b0ab8a37b5
tul-t <tulya.nambi@gmail.com>
2024-10-31 18:51:58 +0530

    Initial Commit

---
+++ EmotionDetection/detector.py
@@ -0,0 +1,64 @@
+from keras.models import load_model
+import numpy as np
+import cv2
+from keras.utils import img_to_array
+from keras.preprocessing import image
+from PIL import ImageGrab
+import sys
+
+arg = sys.argv[1]
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+prediction_model = load_model('model')
+age_model = load_model('age')
+
+if (arg == '-c'):
+    capture = cv2.VideoCapture(0)
+
+emotion_labels = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Neutral', 5:'Sad', 6:'Surprise'}
+colors = {0:(0, 128, 255), 1:(246, 61, 252), 2:(246, 61, 252), 3:(0,255,90), 4:(204, 204, 204), 5:(237, 28, 63), 6:(252,238,33)}
+
+while True:
+    camera = True
+    if (arg == '-c'):
+        _, frame = capture.read()
+
+    else:    
+        camera = False
+        frame = np.array(ImageGrab.grab(bbox=(40,300,1200,1000)))
+        
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        roi = gray[y : y + h, x : x + w]
+        roi = cv2.resize(roi, (48, 48), interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi]) != 0:
+            roi_ = roi.astype('float')/255.0
+            roi_ = img_to_array(roi_)
+            print(roi_.ndim, roi_.shape)
+            roi_ = np.expand_dims(roi_, axis=0)
+            print(roi_.shape)
+
+            prediction = prediction_model.predict(roi_)[0]
+            emotion = emotion_labels[prediction.argmax()]
+            age = str(int(age_model.predict(roi_)))
+            label_pos = (x, y - 10)
+
+            cv2.putText(frame, f'{emotion}, {age}', label_pos, cv2.FONT_HERSHEY_PLAIN, 2, colors[prediction.argmax()], 2)
+
+        else:
+            pass
+    
+    if not camera:
+        cv2.imshow('Detector', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    else:
+        cv2.imshow('Detector', frame)
+
+    if cv2.waitKey(1) == ord('Q'):
+        break
+
+cv2.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,35 @@
+# EmotionDetection
+### High school project on machine learning using TensorFlow, Keras and OpenCV
+<b> A model that recognizes emotions and classifies them into 7 categories.
+
+<b> Dataset used (emotion detection) </b>: Fer13
+<br>
+<b> Final model accuracy (emotion detection) </b>: 74.5% on training set, 64% on test set
+<br>
+<br>
+<b> Dataset used (age detection) </b>: UTKFace
+<br>
+<b> Final model mae (age detection) </b>: 5.6
+
+
+### Download the models here: </i><br>
+  <b> Age Detection model <b>:  https://drive.google.com/file/d/12WBK04uGUh4yYU8WzYbY7ZNapQ6gkGcv/view?usp=share_link <br>
+  <b> Emotion Detection model <b>: https://drive.google.com/file/d/1M9SC4r8e3STFP6CV2lA_FLtEpD9B3W51/view?usp=drive_link
+
+
+### Setup: <br>
+  * Clone the repository
+  * Download both models and place them in the 'EmotionDetection/' directory
+  * Make sure dependencies are installed
+  
+### Usage: <br>
+  <b>To run using streamlit: </b><br>
+  * streamlit run app.py
+
+  <b>To run using terminal: </b><br>
+  * python detector.py -w (for windowed detection mode)
+  * python detector.py -c (for camera detection mode)
+  
+### Dependencies: <br>
+  * TensorFlow, Opencv, Pillow
+  * streamlit (optional)
